16522b6062500c312c712c867756e5fe1bec3167
lowercasewords <87104601+lowercasewords@users.noreply.github.com>
2026-04-14 08:56:48 -0700

    added slowing down the downwards slide once the player lets go of down button

--- scenes/player/states/wall_slide_v.gd
+++ scenes/player/states/wall_slide_v.gd
@@ -2,8 +2,8 @@ class_name WallSlideV extends WallState
 
 const SLIDE_GRAVITY: float = 30.0     # How fast they accelerate downwards once sliding
 const SLIDE_GAVITY_HASTE: float = SLIDE_GRAVITY*10
-const MAX_SLIDE_SPEED: float = 200.0   # The terminal velocity of the slide
-const MAX_SLIDE_SPEED_HASTE: float = MAX_SLIDE_SPEED*1.1   # The terminal velocity of the slide
+const MAX_SLIDE_SPEED: float = 50.0   # The terminal velocity of the slide
+const MAX_SLIDE_SPEED_HASTE: float = MAX_SLIDE_SPEED*4   # The terminal velocity of the slide
 
 var slide_gravity: float = SLIDE_GRAVITY
 var max_slide_speed: float = MAX_SLIDE_SPEED
@@ -11,15 +11,22 @@ var max_slide_speed: float = MAX_SLIDE_SPEED
 func physics_update(_delta: float) -> void:
 	super.physics_update(_delta)
 	
+	# Player wants the player to go down
 	if player.direction_v < 0:
 		slide_gravity = SLIDE_GAVITY_HASTE
 		max_slide_speed = MAX_SLIDE_SPEED_HASTE
+	# Player wants to stay in place
 	else:
-		slide_gravity = SLIDE_GRAVITY
+		# Stabilize the player by reduce the falling speed
+		if player.velocity.y > MAX_SLIDE_SPEED:
+			slide_gravity = SLIDE_GAVITY_HASTE
+		else:
+			slide_gravity = SLIDE_GRAVITY
 		max_slide_speed = MAX_SLIDE_SPEED
 		
 	player.velocity.y = move_toward(player.velocity.y, max_slide_speed, slide_gravity * _delta)
 	
+	print(player.velocity.y)
 	var wall_direction: float = sidewalls_collision_direction()
 	if player.is_on_floor():
 		transitioned.emit(self, StateMachine.LAND)
