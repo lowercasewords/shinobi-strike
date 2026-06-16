57ed520a97d2674ff08afa439cf2f6f7186b96cf
lowercasewords <87104601+lowercasewords@users.noreply.github.com>
2026-06-15 21:43:24 -0700

    adjusted combo A speeds

--- scenes/entity/player/states/Combat/ground_combo_A.gd
+++ scenes/entity/player/states/Combat/ground_combo_A.gd
@@ -43,17 +43,17 @@ func combo_next_attack(current_attack: ATTACK_TYPE) -> bool:
 	
 func start_attack_A() -> bool:
 	player.animated_sprite.play("ground_combo_A_A")
-	player.velocity.x += player.forward_direction * DEFAULT_H_THURST*2
+	player.velocity.x = player.forward_direction * DEFAULT_H_THURST*2
 	return true
 
 func start_attack_B() -> bool:
 	player.animated_sprite.play("ground_combo_A_B")
-	player.velocity.x += player.forward_direction * DEFAULT_H_THURST
+	player.velocity.x = player.forward_direction * DEFAULT_H_THURST
 	return true
 	
 func start_attack_C() -> bool:
 	player.animated_sprite.play("ground_combo_A_C")
-	player.velocity.x += player.forward_direction * DEFAULT_H_THURST/10
+	player.velocity.x = -player.forward_direction * DEFAULT_H_THURST/10
 	return true
 
 func _on_animation_finished():
